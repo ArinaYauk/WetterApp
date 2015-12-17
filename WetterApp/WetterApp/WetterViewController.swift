9cb3e0acf2b3099a306587cfabfd6537d42a2dfc
Arina Yauk <arina.yauk@student.fhws.de>
2015-12-17 10:47:00 +0100

    GUI

---
+++ WetterApp/WetterApp/WetterViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WetterApp
+//
+//  Created by student on 14.12.15.
+//  Copyright © 2015 student. All rights reserved.
+//
+
+import UIKit
+
+class WetterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
