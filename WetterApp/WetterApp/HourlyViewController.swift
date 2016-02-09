ca1242293b6e0c37d2e83c091ebe3fcaa5a8e0e7
Veronika Bogomaz <veronika.bogomaz@student.fhws.de>
2016-02-09 17:29:20 +0100

    HourlyViewController

---
+++ WetterApp/WetterApp/HourlyViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HourlyViewController.swift
+//  WetterApp
+//
+//  Created by student on 09.02.16.
+//  Copyright © 2016 student. All rights reserved.
+//
+import UIKit
+
+
+class HourlyViewController: UITableViewController {
+    
+}
